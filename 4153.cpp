f4441286be904d4b8d028d97c524316e1937af2d
boonsterrrr <boonsterrrr@gmail.com>
2025-01-22 21:19:31 +0900

    직각삼각형형

---
+++ 4153.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//4153
+int main() {
+	int a, b, c;
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) {
+			break;
+		}
+
+		if (a * a + b * b == c * c ||b*b+c*c==a*a ||c*c+a*a==b*b) {
+			cout << "right" << '\n';
+		}
+		else {
+			cout << "wrong" << '\n';
+		}
+	}
+}
